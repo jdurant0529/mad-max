96493718fd538f2d65f3b7b5179bac52cf872742
jdurant0529 <james.durant@gmail.com>
2016-06-27 13:52:59 -0400

    very basic stuff works

--- max.js
+++ max.js
@@ -2,6 +2,8 @@ function max() {
     var mysql = require("mysql");
     var inquirer = require('inquirer');
 
+    var currentVehicle = {};
+
     var vehicleOptions = [{
         type: 'Sedan',
         wheels: '4',
@@ -22,36 +24,62 @@ function max() {
         fuelRemain: '40',
     }]
 
-console.log(vehicleOptions.type);
+    var vehicleTypes = ['Sedan', 'Truck', 'Motorcycle'];
 
-    console.log('choose your vehicle:');
-    inquirer.prompt([
-    	{
-        type: "list",
-        name: "vehicleChoice",
-        message: "What type of vehicle do you want to drive?",
-        choices: [vehicleOptions.type]
-    	}
+    inquirer.prompt([{
+            type: "list",
+            name: "vehicleChoice",
+            message: "What type of vehicle do you want to drive?",
+            choices: vehicleTypes
+        }
 
+    ]).then(function(choice) {
+            console.log("You chose " + choice.vehicleChoice);
 
-    ]).then(function(choice){
-    	console.log("inside choice function" + choice.vehicleChoice);	
-    }
+            switch (choice.vehicleChoice) {
+                case 'Sedan':
+                    currentVehicle = {
+                        type: 'Sedan',
+                        wheels: '4',
+                        tankSize: '20',
+                        oilLife: '100',
+                        fuelRemain: '20',
+                    };
+                    break;
+                case 'Truck':
+                    currentVehicle = {
+                        type: 'Truck',
+                        wheels: '4',
+                        tankSize: '40',
+                        oilLife: '100',
+                        fuelRemain: '40',
+                    }
+                    break;
+                case Motorcycle:
+                    currentVehicle = {
+                        type: 'Motorcycle',
+                        wheels: '2',
+                        tankSize: '15',
+                        oilLife: '100',
+                        fuelRemain: '15',
+                    }
+                    break;
 
+            } // end of case switch statement
 
+           console.log(currentVehicle);
 
+        }
 
-
-); // end of inquirer.prompt to choose vehicle
-
-
-
+    ); // end of inquirer.prompt to choose vehicle
 
     function transport() {
 
     }
 
+    function buyGas(tankSize, inTank) {
+
+    }
 } // end of max function
 
-
-max()
+max() //start mad-max game!!!
